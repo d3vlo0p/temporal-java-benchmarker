1177cf5b15804e01c65bff7234c1cb614202d6bb
d3vlo0p <79021417+d3vlo0p@users.noreply.github.com>
2023-03-16 20:36:20 +0100

    fix poller not starting with 1.18.x version

--- activity-worker/src/main/java/dev/loop/temporalio/benchmarker/activityworker/ActivityWorkerApplication.java
+++ activity-worker/src/main/java/dev/loop/temporalio/benchmarker/activityworker/ActivityWorkerApplication.java
@@ -2,12 +2,14 @@ package dev.loop.temporalio.benchmarker.activityworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ActivityWorkerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ActivityWorkerApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(ActivityWorkerApplication.class, args);
+		context.start();
 	}
 
 }
--- workflow-worker/src/main/java/dev/loop/temporalio/benchmarker/workflowworker/WorkflowWorkerApplication.java
+++ workflow-worker/src/main/java/dev/loop/temporalio/benchmarker/workflowworker/WorkflowWorkerApplication.java
@@ -2,12 +2,14 @@ package dev.loop.temporalio.benchmarker.workflowworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class WorkflowWorkerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorkflowWorkerApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(WorkflowWorkerApplication.class, args);
+		context.start();
 	}
 
 }
